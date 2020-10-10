62f7d0ef3d1eca17d1190c6c6de2b1ee0fb6660f
tesfalem tekie <teshanna72016@gmail.com>
2020-10-10 10:30:34 -0500

    updated file

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 var objPeople = [
+{
 		username: "ramsey",
 		password: "admin"
 	},
-	{
+	{ // Object @ 1 index
 		username: "tekie",
 		password: "admin"
 	},
-	{
+	{ // Object @ 2 index
 		username: "robert",
-		password: "admin"
+		password: "tekie"
 	}
 
 ]
 
 function getInfo() {
-	var username = document.getElementById('username').value
-	var password = document.getElementById('password').value
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
 
 	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
 		if(username == objPeople[i].username && password == objPeople[i].password) {
-			console.log("Log-in successful")
-			window.location.href = "index.html";
+			console.log(username + " is logged in!!!")
+			location.href = 'index.html';
+			// stop the function if this is found to be true
 			return
 		}
 	}
-	console.log("Incorrect username or password")
+	console.log("incorrect username or password")
 }
