ea42710fdb69e559832be2f1e52183471c1f926c
ramseyrr3 <70614059+ramseyrr3@users.noreply.github.com>
2020-10-10 00:47:41 -0500

    javascript updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
-var objUsers = [ 
-	{
-		username:"ramsey",password:"admin"},
-	{
-		username: "tekie",password: "admin"},
-	{
-		username: "robert",password: "admin"}
-	];
+var objPeople = [
+{
+		username: "ramsey",
+		password: "admin"
+	},
+	{ // Object @ 1 index
+		username: "tekie",
+		password: "admin"
+	},
+	{ // Object @ 2 index
+		username: "robert",
+		password: "tekie"
+	}
+
+]
 
 function getInfo() {
-	var username = document.getElementById('username').value
-	var password = document.getElementById('password').value
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
 
-	for(var i = 0; i < objUsers.length; i++) {
-		if(username == objUsers[i].username && password == objUsers[i].password) {
-			console.log("Log-in successful")
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + " is logged in!!!")
+			location.href = 'index.html';
+			// stop the function if this is found to be true
 			return
 		}
 	}
-	console.log("Incorrect username or password")
+	console.log("incorrect username or password")
 }
