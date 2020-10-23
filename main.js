c03f6000181b69c1007762b01cdef01088c87cd4
ramseyrr3 <70614059+ramseyrr3@users.noreply.github.com>
2020-10-23 13:25:46 -0500

    Color schemes edits

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-var objPeople = [
-{
-		username: "ramsey",
-		password: "admin"
-	},
-	{ // Object @ 1 index
-		username: "tekie",
-		password: "admin"
-	},
-	{ // Object @ 2 index
-		username: "robert",
-		password: "tekie"
-	}
 
-]
 
 function getInfo() {
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
+	
+	var us = document.getElementById("username").value;
+    var pw = document.getElementById("password").value;
+    var valid = -1;
+	
+	var usArray = ["ramsey", "tekie", "robert"];
+	var pwArray = ["admin", "admin", "admin"];
 
-	for(var i = 0; i < objPeople.length; i++) {
-		// check is user input matches username and password of a current index of the objPeople array
-		if(username == objPeople[i].username && password == objPeople[i].password) {
-			console.log(username + " is logged in!!!")
-			location.href = 'index.html';
-			// stop the function if this is found to be true
-			return
-		}
-	}
-	console.log("incorrect username or password")
-};
+
+	 for (var i=0; i <usArray.length; i++) {
+        if ((us == usArray[i]) && (pw == pwArray[i])) {
+            valid = i;
+            break;
+        }
+    }
+
+    if (valid != -1) {
+        document.getElementById("password").value = pwArray[valid];
+		location.href = 'index.html';
+		
+    }
+    else {
+        alert("Invalid Username and/or Password! Please try again. You will not be able to submit this form without a successful login")
+        document.getElementById("password").value = "";
+        document.getElementById("username").value = "";
+        document.getElementById("username").focus();
+    }
+	
+}
